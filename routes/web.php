3874a74e05e1a297e2ab47cf86c72dbeea86e5a8
shabazejaz <shabaz@scorchsoft.com>
2021-01-12 09:47:57 +0000

    add web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,13 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
+Route::get('/feed/{id}', [DashboardController::class, 'show'])->middleware(['auth'])->name('feed');
+Route::post('/feed', [DashboardController::class, 'create']);
 
 require __DIR__.'/auth.php';
